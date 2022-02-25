6cc1960361f4a80d6f91e964eb65a5c7daa6abff
Alex <alexiavetere@gmail.com>
2022-02-25 12:13:08 -0300

    1.2

--- main.go
+++ main.go
@@ -1,12 +1,57 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/gocolly/colly"
 )
 
+var (
+	Token string
+)
+
+func init() {
+
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
 func main() {
+
+	// Bot details
+
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Scraping the price
 
 	allFacts := make([]string, 0)
 
@@ -20,5 +65,13 @@ func main() {
 
 	})
 	c.Visit("https://valoruf.cl/")
-	fmt.Println("1 UF =", allFacts[1], "CLP")
+	response := ("1 UF =" + allFacts[1] + "CLP")
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "!UF" {
+		s.ChannelMessageSend(m.ChannelID, response)
+	}
 }
